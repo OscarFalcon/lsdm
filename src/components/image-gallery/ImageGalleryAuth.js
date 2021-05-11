18c6c78abb67193287ccccc020490687e465e97a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-10 22:31:39 -0500

    Fixed ImageGallery call from redirect.

--- src/components/image-gallery/ImageGalleryAuth.js
+++ src/components/image-gallery/ImageGalleryAuth.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import { Redirect } from 'react-router-dom'
 import jwt from 'jsonwebtoken'
-import ImageGallery from './ImageGallery';
 import ImageGalleryContainer from '../../containers/ImageGalleryContainer';
 
 
